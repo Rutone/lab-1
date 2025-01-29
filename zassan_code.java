eaca685009385d47875583fcbf497a3ed2dd2651
Rutone <zaga8037@gmail.com>
2025-01-29 18:30:28 +0800

    Add files via upload

---
+++ zassan_code.java
@@ -0,0 +1,18 @@
+/**
+ * Хоёр тоог нэмж үр дүнг хэвлэнэ.
+ */
+public class zassan_code {
+
+  /**
+     * Энэ функц нь бүртгэгдсэн 2 тоог нэмнэ.
+     */
+  public static void main(String[] args) {
+  
+    int num1 = 5;
+    int num2 = 10;
+
+    int sum = num1 + num2;
+
+    System.out.println("Sum: " + sum);
+  }
+}
